ae759f141ba9de5b02fa4741640a386d0568b8ef
Edyane <edyanealves@gmail.com>
2025-02-23 09:20:36 -0300

    test: buscando um produto e adicionando o mesmo ao carrinho de compras

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -21,4 +21,18 @@ describe("add product to cart", () => {
 
 		cy.contains("Cart (1)").should("exist");
 	});
+
+	it("should be able to search for a product and add it to the cart", () => {
+		cy.visit("http://localhost:3000");
+
+		cy.get("input[name=q]").type("moletom").parent("form").submit();
+
+		cy.get('a[href^="/product"]').first().click();
+
+		cy.location("pathname").should("include", "/product");
+
+		cy.contains("Adicionar ao carrinho").click();
+
+		cy.contains("Cart (1)").should("exist");
+	});
 });
