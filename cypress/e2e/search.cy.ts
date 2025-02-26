7cd9666c5979bde6af238eda72705331db229871
Edyane <edyanealves@gmail.com>
2025-02-25 21:55:46 -0300

    test: verificando se uma busca vazia retorna alguma consulta

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -6,4 +6,12 @@ describe("search products", () => {
 		cy.location("search").should("include", "q=moletom");
 		cy.get('a[href^="/product"]').should("exist");
 	});
+
+	it("should not be able to search page without a search query", () => {
+		cy.on("uncaught:exception", () => {
+			return false;
+		});
+		cy.visit("/search");
+		cy.location("pathname").should("equal", "/");
+	});
 });
